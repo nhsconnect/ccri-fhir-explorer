511734f84695848ce8a2cb0af391c6ec54605920
KevinMayfield <mayfield.g.kev@gmail.com>
2019-03-04 11:07:42 +0000

    Material and theme changes

--- ccri-fhirexplorer/src/main/java/uk/nhs/careconnect/ri/explorer/CamelMonitorRoute.java
+++ ccri-fhirexplorer/src/main/java/uk/nhs/careconnect/ri/explorer/CamelMonitorRoute.java
@@ -21,14 +21,14 @@ public class CamelMonitorRoute extends RouteBuilder {
 
 	private String gpcAuth="Bearer eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwczovL29yYW5nZS50ZXN0bGFiLm5ocy51ay9ncGNvbm5lY3QtZGVtb25zdHJhdG9yL3YxLyIsInN1YiI6IjEiLCJhdWQiOiJodHRwczovL2F1dGhvcml6ZS5maGlyLm5ocy5uZXQvdG9rZW4iLCJleHAiOjE1Mzk1MzA1OTcsImlhdCI6MTUzOTUzMDI5NywicmVhc29uX2Zvcl9yZXF1ZXN0IjoiZGlyZWN0Y2FyZSIsInJlcXVlc3RlZF9zY29wZSI6InBhdGllbnQvKi5yZWFkIiwicmVxdWVzdGluZ19kZXZpY2UiOnsicmVzb3VyY2VUeXBlIjoiRGV2aWNlIiwiaWQiOiIxIiwiaWRlbnRpZmllciI6W3sic3lzdGVtIjoiV2ViIEludGVyZmFjZSIsInZhbHVlIjoiR1AgQ29ubmVjdCBEZW1vbnN0cmF0b3IifV0sInR5cGUiOnsiY29kaW5nIjpbeyJzeXN0ZW0iOiJEZXZpY2VJZGVudGlmaWVyU3lzdGVtIiwiY29kZSI6IkRldmljZUlkZW50aWZpZXIifV19LCJtb2RlbCI6InYxIiwidmVyc2lvbiI6IjEuMSJ9LCJyZXF1ZXN0aW5nX29yZ2FuaXphdGlvbiI6eyJyZXNvdXJjZVR5cGUiOiJPcmdhbml6YXRpb24iLCJpZCI6IjEiLCJpZGVudGlmaWVyIjpbeyJzeXN0ZW0iOiJodHRwczovL2ZoaXIubmhzLnVrL0lkL29kcy1vcmdhbml6YXRpb24tY29kZSIsInZhbHVlIjoiW09EU0NvZGVdIn1dfSwicmVxdWVzdGluZ19wcmFjdGl0aW9uZXIiOnsicmVzb3VyY2VUeXBlIjoiUHJhY3RpdGlvbmVyIiwiaWQiOiIxIiwiaWRlbnRpZmllciI6W3sic3lzdGVtIjoiaHR0cHM6Ly9maGlyLm5ocy51ay9JZC9zZHMtdXNlci1pZCIsInZhbHVlIjoiRzEzNTc5MTM1In0seyJzeXN0ZW0iOiJsb2NhbFN5c3RlbSIsInZhbHVlIjoiMSJ9XSwibmFtZSI6W3siZmFtaWx5IjoiRGVtb25zdHJhdG9yIiwiZ2l2ZW4iOlsiR1BDb25uZWN0Il0sInByZWZpeCI6WyJNciJdfV19fQ.";
 
-	@Value("${jolokia.jmxendpoint.ccriserver}")
-	private String jmxCCRIServer;
+	@Value("${jolokia.jmxendpoint.ccportal}")
+	private String jmxCCPortal;
 
 	@Value("${jolokia.jmxendpoint.ccridocument}")
 	private String jmxCCRIDocument;
 
-	@Value("${jolokia.jmxendpoint.ccriintegration}")
-	private String jmxCCRIIntegration;
+	@Value("${jolokia.jmxendpoint.ccrimessaging}")
+	private String jmxCCRIMessaging;
 
 	@Value("${fhir.resource.serverBase}")
 	private String serverBase;
@@ -81,13 +81,13 @@ public class CamelMonitorRoute extends RouteBuilder {
 
 
 
-		from("servlet:ccri-fhirserver?matchOnUriPrefix=true")
-				.routeId("ccri-fhiserver-jokolia")
-			.to(jmxCCRIServer);
+		from("servlet:cc-portal?matchOnUriPrefix=true")
+				.routeId("cc-portal-jokolia")
+			.to(jmxCCPortal);
 
-		from("servlet:ccri-integration?matchOnUriPrefix=true")
-				.routeId("ccri-integration-jokolia")
-				.to(jmxCCRIIntegration);
+		from("servlet:ccri-messaging?matchOnUriPrefix=true")
+				.routeId("ccri-messaging-jokolia")
+				.to(jmxCCRIMessaging);
 
 		from("servlet:ccri-document?matchOnUriPrefix=true")
 				.routeId("ccri-document-jokolia")
