b8303350d06c597b177190a595888292703b3eb3
KevinMayfield <mayfield.g.kev@gmail.com>
2019-06-27 07:27:29 +0100

    Cors configuration

--- ccri-fhirexplorer/src/main/java/uk/nhs/careconnect/ri/explorer/ExplorerApp.java
+++ ccri-fhirexplorer/src/main/java/uk/nhs/careconnect/ri/explorer/ExplorerApp.java
@@ -7,8 +7,12 @@ import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.spring.boot.CamelContextConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class ExplorerApp {
@@ -40,6 +44,29 @@ public class ExplorerApp {
 
             }
         };
+    }
+
+    @Bean
+    CorsConfigurationSource
+    corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter());
+        bean.setOrder(0);
+        return bean;
     }
 
 }
